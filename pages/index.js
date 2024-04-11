27c9f013147a3c1d2a48c74344e02e94ffbb0441
agarjoshua <joshuaagarjj@gmail.com>
2024-04-11 21:32:31 +0300

    changed class to classname

--- pages/index.js
+++ pages/index.js
@@ -121,20 +121,20 @@ export default function Home() {
         <div className="mt-10 tablet:ml-12 laptop:ml-11 laptopl:ml-11 mobs:ml-4">
           <h1 className="text-2xl font-bold">Experience</h1>
           <div className="mt-1">
-            <div class="w-full flex space-x-3 p-10 snap-x snap-mandatory mt-1 md:mt-28 scrollbar-thin overflow-y-hidden overflow-x-scroll scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
-              <article class="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
-                <div class="px-2 md:px-10">
-                  <h4 class="text-lg md:text-2xl lg:text-4xl font-light">
+            <div className="w-full flex space-x-3 p-10 snap-x snap-mandatory mt-1 md:mt-28 scrollbar-thin overflow-y-hidden overflow-x-scroll scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+              <article className="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
+                <div className="px-2 md:px-10">
+                  <h4 className="text-lg md:text-2xl lg:text-4xl font-light">
                     Principal I.C.T. Officer - (Software & Applications
                     Engineer)
                   </h4>
-                  <p class="font-bold text-lg md:text-2xl md:mt-1">
+                  <p className="font-bold text-lg md:text-2xl md:mt-1">
                     Lake Basin Development Authority
                   </p>
-                  <p class="uppercase py-3 md:py-5 text-gray-300">
+                  <p className="uppercase py-3 md:py-5 text-gray-300">
                     August 2021 - Present{" "}
                   </p>
-                  <ul class="list-disc space-y-4 ml-5 text-sm md:text-lg">
+                  <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
                     <li>
                       Digitized manual processes by implementing a process
                       management schedule systems
@@ -156,18 +156,18 @@ export default function Home() {
                   </ul>
                 </div>
               </article>
-              <article class="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
-                <div class="px-2 md:px-10">
-                  <h4 class="text-lg md:text-2xl lg:text-4xl font-light">
+              <article className="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
+                <div className="px-2 md:px-10">
+                  <h4 className="text-lg md:text-2xl lg:text-4xl font-light">
                     Full Stack Engineer (Consulting)
                   </h4>
-                  <p class="font-bold text-lg md:text-2xl md:mt-1">
+                  <p className="font-bold text-lg md:text-2xl md:mt-1">
                     iSOFT Systems
                   </p>
-                  <p class="uppercase py-3 md:py-5 text-gray-300">
+                  <p className="uppercase py-3 md:py-5 text-gray-300">
                     August 2021 - Present
                   </p>
-                  <ul class="list-disc space-y-4 ml-5 text-sm md:text-lg">
+                  <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
                     <li>
                       Successfully developed, tested, and implemented a web
                       based learning information management system
@@ -186,18 +186,18 @@ export default function Home() {
                   </ul>
                 </div>
               </article>
-              <article class="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
-                <div class="px-2 md:px-10">
-                  <h4 class="text-lg md:text-2xl lg:text-4xl font-light">
+              <article className="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
+                <div className="px-2 md:px-10">
+                  <h4 className="text-lg md:text-2xl lg:text-4xl font-light">
                     Backend Software Engineer
                   </h4>
-                  <p class="font-bold text-lg md:text-2xl md:mt-1">
+                  <p className="font-bold text-lg md:text-2xl md:mt-1">
                     Businesscom Consulting Services
                   </p>
-                  <p class="uppercase py-3 md:py-5 text-gray-300">
+                  <p className="uppercase py-3 md:py-5 text-gray-300">
                     June 2021 - Sep 2021
                   </p>
-                  <ul class="list-disc space-y-4 ml-5 text-sm md:text-lg">
+                  <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
                     <li>
                       Developed a demand generation application, increasing
                       client acquisition.
@@ -221,18 +221,18 @@ export default function Home() {
                   </ul>
                 </div>
               </article>
-              <article class="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
-                <div class="px-2 md:px-10">
-                  <h4 class="text-lg md:text-2xl lg:text-4xl font-light">
+              <article className="flex flex-col rounded-lg items-center space-y-12 md:space-y-7 flex-shrink-0 w-1/2 snap-start bg-[#292929] p-4 md:p-10 hover:opacity-100 opacity-60 cursor-pointer transition-opacity duration-200 tablet:w-2/4 mob:w-full mobs:w-full">
+                <div className="px-2 md:px-10">
+                  <h4 className="text-lg md:text-2xl lg:text-4xl font-light">
                     Software Development Engineer in Test (Intern)
                   </h4>
-                  <p class="font-bold text-lg md:text-2xl md:mt-1">
+                  <p className="font-bold text-lg md:text-2xl md:mt-1">
                     Safaricom PLC
                   </p>
-                  <p class="uppercase py-3 md:py-5 text-gray-300">
+                  <p className="uppercase py-3 md:py-5 text-gray-300">
                     Feb 2022-Apr 2022
                   </p>
-                  <ul class="list-disc space-y-4 ml-5 text-sm md:text-lg">
+                  <ul className="list-disc space-y-4 ml-5 text-sm md:text-lg">
                     <li>
                       Performed integration, unit, and functional testing,
                       ensuring the launch of a service management system with
@@ -258,28 +258,28 @@ export default function Home() {
         <div className="mt-10 tablet:ml-12 laptop:ml-11 laptopl:ml-11 mobs:ml-4">
           <h1 className="text-2xl font-bold">Skills</h1>
           <div className="mt-1">
-            <div class="flex flex-col space-y-2">
-              <div class="grid grid-cols-1 md:grid-cols-2 gap-4 tablet:grid-cols-4">
+            <div className="flex flex-col space-y-2">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4 tablet:grid-cols-4">
                 <div>
-                  <p class="text-gray-300 font-bold">Programming Languages:</p>
-                  <ul class="list-disc space-y-1 pl-4">
+                  <p className="text-gray-300 font-bold">Programming Languages:</p>
+                  <ul className="list-disc space-y-1 pl-4">
                     <li>Python</li>
                     <li>JavaScript</li>
                     <li>Dart (familiar)</li>
                   </ul>
 
-                  <p class="text-gray-300 font-bold mt-4 ">Databases:</p>
-                  <ul class="list-disc space-y-1 pl-4">
+                  <p className="text-gray-300 font-bold mt-4 ">Databases:</p>
+                  <ul className="list-disc space-y-1 pl-4">
                     <li>SQL, Postgres</li>
                     <li>SQLite</li>
                   </ul>
                 </div>
 
                 <div>
-                  <p class="text-gray-300 font-bold">
+                  <p className="text-gray-300 font-bold">
                     Frameworks and technologies:
                   </p>
-                  <ul class="list-disc space-y-1 pl-4">
+                  <ul className="list-disc space-y-1 pl-4">
                     <li>Django, FastAPI</li>
                     <li>Vue.js, React</li>
                     <li>Strapi, wordpress, Wagtail</li>
@@ -289,8 +289,8 @@ export default function Home() {
                 </div>
 
                 <div>
-                  <p class="text-gray-300 font-bold">Cloud:</p>
-                  <ul class="list-disc space-y-1 pl-4">
+                  <p className="text-gray-300 font-bold">Cloud:</p>
+                  <ul className="list-disc space-y-1 pl-4">
                     <li>Google Cloud Platform (GCP), Azure</li>
                   </ul>
                 </div>
@@ -299,74 +299,74 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="mt-10 tablet:ml-12 laptop:ml-11 laptopl:ml-11 mobs:ml-4">
+        <div className="mt-10 tablet:ml-12 laptop:ml-11 laptopl:ml-11 mobs:ml-4">
           <h1 className="text-2xl font-bold">Projects</h1>
 
-          <div class="grid grid-cols-1 sm:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-8 max-w-12xl mx-auto lg:gap-x-20 mt-3">
-              <div class="github-repo">
-                <h1 class="font-semibold text-xl dark:text-gray-200 text-gray-700">
+          <div className="grid grid-cols-1 sm:grid-cols-2 tablet:grid-cols-3 laptop:grid-cols-4 gap-8 max-w-12xl mx-auto lg:gap-x-20 mt-3">
+              <div className="github-repo">
+                <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
                   iSOFT Learning information Management System
                 </h1>
-                <p class="text-base font-normal my-4 text-gray-500">
+                <p className="text-base font-normal my-4 text-gray-500">
                   A fullstack learning information management system built on django.
                 </p>
                 <a
                   href="https://isoft.azurewebsites.net/"
-                  class="font-semibold group flex flex-row space-x-2 w-full items-center"
+                  className="font-semibold group flex flex-row space-x-2 w-full items-center"
                 >
                   <p>View App </p>
-                  <div class="transform group-hover:translate-x-2 transition duration-300">
+                  <div className="transform group-hover:translate-x-2 transition duration-300">
                     →
                   </div>
                 </a>
               </div>
-              <div class="github-repo">
-                <h1 class="font-semibold text-xl dark:text-gray-200 text-gray-700">
+              <div className="github-repo">
+                <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
                   whip-backend
                 </h1>
-                <p class="text-base font-normal my-4 text-gray-500">
+                <p className="text-base font-normal my-4 text-gray-500">
                   A fastapi backend service that manages a land access management crud functionality
                 </p>
                 <a
                   href="https://github.com/agarjoshua/whip-backend"
-                  class="font-semibold group flex flex-row space-x-2 w-full items-center"
+                  className="font-semibold group flex flex-row space-x-2 w-full items-center"
                 >
                   <p>View Repository </p>
-                  <div class="transform group-hover:translate-x-2 transition duration-300">
+                  <div className="transform group-hover:translate-x-2 transition duration-300">
                     →
                   </div>
                 </a>
               </div>
-              <div class="github-repo">
-                <h1 class="font-semibold text-xl dark:text-gray-200 text-gray-700">
+              <div className="github-repo">
+                <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
                   Provasi
                 </h1>
-                <p class="text-base font-normal my-4 text-gray-500">
+                <p className="text-base font-normal my-4 text-gray-500">
                       A frontend realtor service built on vue.js that uses google maps to help map location of properties
                 </p>
                 <a
                   href="https://github.com/agarjoshua/provasi/"
-                  class="font-semibold group flex flex-row space-x-2 w-full items-center"
+                  className="font-semibold group flex flex-row space-x-2 w-full items-center"
                 >
                   <p>View Repository </p>
-                  <div class="transform group-hover:translate-x-2 transition duration-300">
+                  <div className="transform group-hover:translate-x-2 transition duration-300">
                     →
                   </div>
                 </a>
               </div>
-              <div class="github-repo">
-                <h1 class="font-semibold text-xl dark:text-gray-200 text-gray-700">
+              <div className="github-repo">
+                <h1 className="font-semibold text-xl dark:text-gray-200 text-gray-700">
                   Porfolio Website
                 </h1>
-                <p class="text-base font-normal my-4 text-gray-500">
+                <p className="text-base font-normal my-4 text-gray-500">
                       A fun project I'm currently working on to present a friends portfolio
                 </p>
                 <a
                   href="https://portfolio-ashen-five-61.vercel.app/"
-                  class="font-semibold group flex flex-row space-x-2 w-full items-center"
+                  className="font-semibold group flex flex-row space-x-2 w-full items-center"
                 >
                   <p>View Repository </p>
-                  <div class="transform group-hover:translate-x-2 transition duration-300">
+                  <div className="transform group-hover:translate-x-2 transition duration-300">
                     →
                   </div>
                 </a>
